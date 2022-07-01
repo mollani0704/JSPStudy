673b984dc30f145e2eaf30ea64eb532733a07fc4
mollani <mollani0704@gmail.com>
2022-07-01 22:05:59 +0900

    [웹개발] 22.07.01 데이터베이스 자바 연결

---
+++ jspsutdy/src/main/java/service/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package service;
+
+import domain.dao.UserDao;
+import domain.dao.UserDaoImpl;
+import web.dto.SignupReqDto;
+
+public class UserServiceImpl implements UserService{
+	
+	private final UserDao userDao;
+	
+	public UserServiceImpl() {
+		userDao = new UserDaoImpl();
+	}
+
+	@Override
+	public boolean createUser(SignupReqDto signupReqDto) throws Exception {
+		return userDao.save(signupReqDto.toEntity()) > 0;
+	}
+
+	@Override
+	public void getUser() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean updateUser() throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteUser() throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
---
+++ jspsutdy/src/main/java/web/dto/SignupReqDto.java
@@ -0,0 +1,26 @@
+package web.dto;
+
+import domain.entity.User;
+import lombok.Builder;
+import lombok.Data;
+import service.UserService;
+
+@Data
+@Builder
+public class SignupReqDto {
+	private String name;
+	private String email;
+	private String username;
+	private String password;
+	
+	public User toEntity() {
+		return User.builder()
+				.name(name)
+				.email(email)
+				.username(username)
+				.password(password)
+				.roles("ROLE_USER")
+				.build();
+		
+	}
+}
